36c47158ecb28d4969e1401697ae5801ac260dab
Onsel <onselaydin@gmail.com>
2018-01-17 13:42:54 +0300

    Object and Functions
    Object and Functions

---
+++ section5.js
@@ -0,0 +1,13 @@
+var onsel={
+    name: 'Onsel',
+    yearofBirth: 1975,
+    job: 'Software Developer'
+};
+
+var Person=function(name,yearofBirth,job){
+    this.name=name;
+    this.yearofBirth=yearofBirth;
+    this.job=job;
+}
+
+var yagmur=new Person('Yagmur',2008,'student');
