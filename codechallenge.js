b26b7b07666590c398ac9ab72b9eba4eafa1181e
Onsel <onselaydin@gmail.com>
2018-01-28 21:10:29 +0300

    new project
    new project

--- codechallenge.js
+++ codechallenge.js
@@ -13,14 +13,23 @@ Question.prototype.displayQuestion=
         }
     }
 
-Question.prototype.checkAnswer=function(ans){
+Question.prototype.checkAnswer=function(ans,callback){
     if(ans===this.correct){
+        var sc;
         console.log('Correct answer!');
+        sc=callback(true);
     }else{
         console.log('Wrong answer. Try again :)');
+        sc=callback(false);
     }
+    this.displayScore(sc);
 }
 
+Question.prototype.displayScore=
+    function(score){
+    console.log('Your current score is: ' + score);
+    console.log('-----------------------------');
+}
 
 var q1=new Question('Is JavaScript the coolest programming langulage in the world?', 
                    ['Yes','No'],0);
@@ -30,6 +39,18 @@ var q2=new Question('What is the name this course\'s teacher?',
 
 var q3=new Question('What does best describe coding',
                    ['Boring','hard','Fun','Tediuos'],2);
+var questions=[q1,q2,q3];
+    function score(){
+        var sc=0;
+        return function(correct){
+            if(correct){
+                sc++;
+            }
+            return sc;
+        }
+    }
+    
+var keepScore=score(); 
 
 function nextQuestion(){
         
@@ -38,9 +59,9 @@ function nextQuestion(){
 
     questions[n].displayQuestion();
 
-    var answer=parseInt(prompt('Please select the correct answer'));
+    var answer=prompt('Please select the correct answer');
 
-    questions[n].checkAnswer(answer);
+    questions[n].checkAnswer(parseInt(answer),keepScore);
     
     
     if(answer!=='exit'){
